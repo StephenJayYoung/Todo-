abbe0b9512cf9d563735a6cc4ded9f172a6aed62
Stephen Young <stephenjayyoung@gmail.com>
2015-10-02 14:03:57 -0700

    added loginview and models

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
 var app = {};
 
 $(function() { //when DOM is ready...
-	app.users = new UserCollection([
-		{username:'Person1', password: '1234'},
-		{username:'Person2', password: '12345'},
-		{username:'Person3', password: '123456'}
+	app.users = new UserCollection(
+
+);
+
+
+	app.tasks = new IssueCollection([
+
 	]);
-
-	app.issues = new IssueCollection([
-		{title:'task1 title',
-		description:'task1 description',
-		status:'assigned'},
-		{title:'task2 title',
-		description:'task1 description',
-		status:'unassigned'},
-		{title:'task3 title',
-		description:'task3 description',
-		status:'assigned'}
-	]);
-
-app.gui = new GUI(app.users,
-					app.issues,
-					'#app');// selector of main div
-
-})
+	app.gui = new GUI(app.users,
+						app.tasks,
+						'#app');
+});
--- views.js
+++ views.js
@@ -1,9 +1,66 @@
 var GUI = (function(){ //IIFE for all Views
 
-
-var LoginView = Backbone.View.extend ({
+//Code for the Login View ////////////////////////////////
+var LoginView = Backbone.View.extend({
+  id : 'LoginView',
+	render: function() {
+		var button = '<button id = "login">Login</button>';
+    // app.users.fetch();
+		var users = app.users.pluck("username");
+    // console.log('client sees these users',app.users);
+		var dropdown = '<select id = "dropdown">';
+    users.forEach(function(element){dropdown += "<option>"+element+"</option>";});
+		dropdown += ('</select>');
+		var title = '<h1>Please Choose A Username</h1>';
+    var title1 = '<br><br><h3>Not A User? Sign Up:</h3>';
+    var form = '<form id = "form"><input id = "input" placeholder="Add a user"></input><button type = "submit" id = "submit">Submit</button></form>';
+		var all =  title + dropdown + button + title1 + form;
+		this.$el.html(  all );
+	},
+	delete: function() {
+			this.$el.html('');
+	},
+	initialize: function() {
+		this.listenTo(app.users, "update", this.render);
+    // this.listenTo(app.users, 'change', this.render);
+    app.users.fetch();
+    app.tasks.fetch();
+    // app.users.invoke('save');
+    this.render();
+	},
+  display : function() {
+    console.log("you are clicking login");
+     app.router.navigate('loggedin', true);
+     this.remove();
+  },
+	events: {
+		"click #logout" : "logout",
+		"click #login" : "login",
+    'click #submit' : "newUser",
+    // 'click #login' : 'display'
+	},
+	login: function() {
+		app.currentUser = $('#dropdown').val();
+    this.remove();
+			app.gui.switchToUser();
+	},
+	logout: function() {
+		this.$el.empty();
+		this.remove();
+		app.gui.switchToLogin();
+	},
+  newUser : function (event) {
+    console.log('newUser event triggered');
+    event.preventDefault();
+    var username = $('#input').val();
+    // app.users.fetch();
+    app.users.create({username: username });
+    // app.users.save();
+  }
 
 });
+
+//End Code for Login View//////////////////////////////
 
 function GUI(users,issues,el) {
 
